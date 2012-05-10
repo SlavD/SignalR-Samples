5266eadb87a97c6c497ac41a1394ab8e6f4173ff
sdorzak <sdorzak@gmail.com>
2012-05-10 21:35:32 +0100

    added sample code to reproduce SignalR connection issue

---
+++ ConnectionIssueTest/MvcApplication4/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            //TestHub.Notify("");
+            return View();
+        }
+
+        [HttpPost]
+        public PartialViewResult Notify()
+        {
+            TestHub.Notify("");
+         
+            return null;
+        }
+    }
+}
---
+++ ConnectionIssueTest/MvcApplication4/SignalR/TestHub.cs
@@ -0,0 +1,17 @@
+﻿using SignalR.Hubs;
+using SignalR;
+using System;
+
+[HubName("testHub")]
+public class TestHub : Hub {
+
+    public static void Notify(string message) {
+        var hubContext = GlobalHost.ConnectionManager.GetHubContext<TestHub>();
+        hubContext.Clients.notify(DateTime.Now);
+    }
+
+    public void Join(string id)
+    {
+        this.Groups.Add(this.Context.ConnectionId, id);
+    }
+}
